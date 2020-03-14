68a5ace09af2431006ed51a6fd901c2111cefa28
Lucas Rodrigues <lucasfr@progammer.net>
2020-03-14 11:12:05 +0000

    feat: add user card component and sarch bar component

---
+++ src/components/User/User.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+import { UserCard, Divider } from './User.style';
+
+const User = ({ user }) => {
+  const [showCatchPhrase, setShowCatchPhrase] = useState(false);
+
+  return user ? (
+    <UserCard>
+      <h2>General Information</h2>
+      <div>Name: {user.name}</div>
+      <div>Email: {user.email}</div>
+      <Divider />
+      <h2>Address</h2>
+      <div>Street: {`${user.address.street}, ${user.address.suite || ''}`}</div>
+      <div>City: {user.address.city}</div>
+      <div>Zip Code: {user.address.zipcode || ''}</div>
+      <Divider />
+      <h2>WebSite</h2>
+      <div>{user.website}</div>
+      <Divider />
+
+      <div
+        className="company-section"
+        onMouseEnter={() => setShowCatchPhrase(true)}
+        onMouseLeave={() => setShowCatchPhrase(false)}
+      >
+        <h2>Company</h2>
+        <div>{user.company.name}</div>
+        <Divider />
+        {showCatchPhrase && user.company.catchPhrase}
+      </div>
+    </UserCard>
+  ) : (
+    <div>User Not Found</div>
+  );
+};
+
+User.prototype = {
+  user: PropTypes.object.isRequired,
+};
+
+export default User;
